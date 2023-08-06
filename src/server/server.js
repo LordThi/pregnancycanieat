e99ca77fbb40321af8476d0d7aee03f0298b75e9
Thibaut Livigni <lordthi@macbook-pro-de-thibaut.home>
2023-08-06 19:26:59 +0200

    🆙[Server] Update server: Add product router

--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const productRouter = require('/Routes/productRouter');
+
+app.use('/api/product', productRouter);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
